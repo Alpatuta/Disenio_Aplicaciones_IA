2bacb8554d1ee11fab1c2ccaa16e4f957681ebc1
foteiza <fede.oteiza@gmail.com>
2026-03-17 10:25:42 -0300

    Agregando saludar con nombre

--- Proyectos/primerProyecto/src/main/java/ort/da/primerProyecto/controllers/HolaMundoController.java
+++ Proyectos/primerProyecto/src/main/java/ort/da/primerProyecto/controllers/HolaMundoController.java
@@ -2,6 +2,7 @@ package ort.da.primerProyecto.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,13 @@ public class HolaMundoController {
     
     public String saludar() {
         return "Hola Mundo con spring boot!";
+    }
+
+    
+    
+    @GetMapping("/saludarconnombre")
+    
+    public String saludarconnombre(@RequestParam String nombre) {
+        return "Hola " + nombre + " con spring boot!";
     }
 }
