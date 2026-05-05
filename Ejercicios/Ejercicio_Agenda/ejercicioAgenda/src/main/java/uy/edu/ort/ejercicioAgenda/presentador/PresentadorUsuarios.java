02fd506d3dc159467c83f18851af1cee1a14d001
foteiza <fede.oteiza@gmail.com>
2026-05-05 15:59:18 -0300

    ADD: Implementar clase Command y métodos en Commands para crear y agregar comandos; actualizar PresentadorUsuarios para manejar login y logout

--- Ejercicios/Ejercicio_Agenda/ejercicioAgenda/src/main/java/uy/edu/ort/ejercicioAgenda/presentador/PresentadorUsuarios.java
+++ Ejercicios/Ejercicio_Agenda/ejercicioAgenda/src/main/java/uy/edu/ort/ejercicioAgenda/presentador/PresentadorUsuarios.java
@@ -1,16 +1,29 @@
 package uy.edu.ort.ejercicioAgenda.presentador;
 
-import uy.edu.ort.ejercicioAgenda.dto.UsuarioDto;
+import uy.edu.ort.ejercicioAgenda.dominio.Command;
 import uy.edu.ort.ejercicioAgenda.dominio.Commands;
+import uy.edu.ort.ejercicioAgenda.dominio.Fachada;
+import uy.edu.ort.ejercicioAgenda.dominio.Usuario;
+import uy.edu.ort.ejercicioAgenda.exception.CredencialesNoValidasException;
 
 public class PresentadorUsuarios {
 
-	public Commands login(String nombre, String contrasenia) {
-		return null;
+	Fachada f;
+
+	public Commands login(String nombre, String contrasenia) throws CredencialesNoValidasException {
+		Commands c = null;
+		Usuario u = f.buscarUsuario(nombre, contrasenia);
+		if (u == null) {
+			throw new CredencialesNoValidasException("Credenciales no válidas");
+		} else {
+			c = Commands.create(new Command("Login exitoso", u));
+		}
+
+		return c;
 	}
 
 	public Commands logout() {
-		return null;
+		return Commands.create(new Command("Logout", null));
 	}
 
 }
